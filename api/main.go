c445e1485ef864e0bbd4174d973105e6759c9b15
yongwoon <yonyon.japan@gmail.com>
2020-06-06 17:05:32 +0900

    Log を file に書き出す

--- api/main.go
+++ api/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/yongwoon/gin-blog/config/initializer"
 	"github.com/yongwoon/gin-blog/db"
+	"github.com/yongwoon/gin-blog/middleware"
 	"github.com/yongwoon/gin-blog/router"
 )
 
 func main() {
 	// 環境変数ロード
 	initializer.InitDotenv()
+
 	// DB 初期化
 	dbConn := db.Init()
+
+	engine := gin.Default()
+
+	// set logger
+	engine.Use(middleware.LoggerToFile())
+
+	// Set cors
+	// @see https://github.com/gin-contrib/cors
+	engine.Use(cors.Default())
+
 	// Router 初期化
-	router.Router(dbConn)
+	router.Router(engine, dbConn)
 }
--- api/router/router.go
+++ api/router/router.go
@@ -1,20 +1,14 @@
 package router
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	v1 "github.com/yongwoon/gin-blog/router/v1"
 )
 
 // Router router list
-func Router(dbConn *gorm.DB) {
-	r := gin.Default()
+func Router(engine *gin.Engine, dbConn *gorm.DB) {
+	v1.InitRouter(engine, dbConn)
 
-	// Set cors
-	// @see https://github.com/gin-contrib/cors
-	r.Use(cors.Default())
-	v1.InitRouter(r, dbConn)
-
-	r.Run(":3001")
+	engine.Run(":3001")
 }
