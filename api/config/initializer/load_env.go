692d2ecb3073a0bf5eb30ec74b9d2b49da737130
yongwoon <yonyon.japan@gmail.com>
2020-06-06 16:22:09 +0900

    Setup dotenv

---
+++ api/config/initializer/load_env.go
@@ -0,0 +1,14 @@
+package initializer
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func InitDotenv() {
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+}
