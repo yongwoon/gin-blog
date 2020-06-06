083b49fb53fe90552285b9cb1ca25ffe472fd33d
yongwoon <yonyon.japan@gmail.com>
2020-06-06 16:22:40 +0900

    Apply dotenv

--- api/db/db.go
+++ api/db/db.go
@@ -1,16 +1,18 @@
 package db
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func Init() *gorm.DB {
 	DBMS := "mysql"
-	USER := "root"
-	PASS := ""
+	USER := os.Getenv("DATABASE_USER")
+	PASS := os.Getenv("DATABASE_PASSWORD")
 	PROTOCOL := "tcp(db:3306)"
-	DBNAME := "blog_development"
+	DBNAME := os.Getenv("DATABASE_SCHEMA")
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
 	db, err := gorm.Open(DBMS, CONNECT)
 
